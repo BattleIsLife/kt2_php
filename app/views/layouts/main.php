356c093b4c9467c99c0e240f37ce5ed1448bbcae
BattleIsLife <nothinginterested123@gmail.com>
2026-04-11 17:59:26 +0700

    Thêm vào style và javascript của  bootstrap 5 cho layout chung

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $WEBSITE_TITLE ?? 'Tiêu đề' ?></title>
+    <link rel="stylesheet" href="css/body.css">
+    <link rel="stylesheet" href="css/bootstrap-5/bootstrap.min.css">
 </head>
 <body>
     <?= $WEBSITE_CONTENT ?? '' ?>
 </body>
+<script src="js/bootstrap-5/bootstrap.min.js"></script>
 </html>
