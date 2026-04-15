4122b886f400ac8c9b5625f65d8fd18421a6126e
vuthaohien15092005-web <vuthaohien15092005@gmail.com>
2026-04-15 21:10:50 +0700

    Sửa một số lỗi về vị trí code

--- app/views/layouts/footer.php
+++
@@ -1,6 +0,0 @@
-</div>
-
-<script src="<?= BASE_URL ?>/js/bootstrap-5/bootstrap.bundle.min.js"></script>
-<script src="<?= BASE_URL ?>/js/product.js"></script>
-</body>
-</html>
--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -3,12 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $WEBSITE_TITLE ?? 'Tiêu đề' ?></title>
-    <link rel="stylesheet" href="css/body.css">
-    <link rel="stylesheet" href="css/bootstrap-5/bootstrap.min.css">
+    <title>ElectroShop - Quản lý sản phẩm</title>
+
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/bootstrap-5/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/body.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/layout.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>/css/product.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
     <?= $WEBSITE_CONTENT ?? '' ?>
 </body>
 <script src="js/bootstrap-5/bootstrap.min.js"></script>
+<script src="<?= BASE_URL ?>/js/bootstrap-5/bootstrap.bundle.min.js"></script>
 </html>
